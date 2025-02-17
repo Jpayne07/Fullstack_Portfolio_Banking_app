0be444189230fd46558ca9e8a55d4ba8cd8c2193
Jacob <jacobpayne007@gmail.com>
2025-02-16 23:54:44 -0600

    various adjustments

--- server/config.py
+++ server/config.py
@@ -24,8 +24,8 @@ app.secret_key = os.environ.get('APP_SECRET_KEY')
 app.config['PERMANENT_SESSION_LIFETIME'] = timedelta(days=1)
 app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URI')
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-app.config['SESSION_COOKIE_SAMESITE'] = 'None'
-app.config['SESSION_COOKIE_SECURE'] = True
+app.config['SESSION_COOKIE_SAMESITE'] = 'Lax'
+app.config['SESSION_COOKIE_SECURE'] = False
 
 # Set session configuration
 app.config['SESSION_TYPE'] = 'sqlalchemy'
