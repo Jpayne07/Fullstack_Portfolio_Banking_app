62c28c00f4eda9ddc7da0b14fabf40d8a2cb9744
Jacob <jacobpayne007@gmail.com>
2025-02-17 01:39:20 -0600

    reworking for solo build

--- server/app.py
+++ server/app.py
@@ -329,4 +329,5 @@ api.add_resource(Insights, '/api/insights')
 
 
 if __name__ == '__main__':
-    app.run(host="0.0.0.0", port=5000)
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host="0.0.0.0", port=port)
