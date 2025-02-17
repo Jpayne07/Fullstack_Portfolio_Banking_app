7d4b0f20692b52085d1ceff9fae9ad4ecf5b7c9f
Jacob <jacobpayne007@gmail.com>
2025-02-16 21:52:37 -0600

    new config

--- server/config.py
+++ server/config.py
@@ -8,9 +8,11 @@ import os
 from dotenv import load_dotenv
 from flask_cors import CORS
 from datetime import timedelta
+from flask_session import Session
 
 
 
+load_dotenv()
 app = Flask(
     __name__,
     static_url_path='',
@@ -20,18 +22,20 @@ app = Flask(
 CORS(app, supports_credentials=True)
 app.secret_key = os.environ.get('APP_SECRET_KEY')
 app.config['PERMANENT_SESSION_LIFETIME'] = timedelta(days=1)
-load_dotenv()
 app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URI')
-# app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///app.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+# Set session configuration
 app.config['SESSION_TYPE'] = 'sqlalchemy'
 app.config['PERMANENT_SESSION_LIFETIME'] = timedelta(days=1)
+
+# Tell Flask-Session to use your existing SQLAlchemy db instance
+
 GITHUB_CLIENT_ID = os.getenv("GITHUB_CLIENT_ID")
 GITHUB_CLIENT_SECRET = os.getenv("GITHUB_CLIENT_SECRET")
 GITHUB_AUTH_URL = "https://github.com/login/oauth/authorize"
 GITHUB_TOKEN_URL = "https://github.com/login/oauth/access_token"
 GITHUB_API_URL = "https://api.github.com/user"
-bcrypt = Bcrypt(app)
 convention = {
     "ix": 'ix_%(column_0_label)s',
     "uq": "uq_%(table_name)s_%(column_0_name)s",
@@ -39,10 +43,15 @@ convention = {
     "fk": "fk_%(table_name)s_%(column_0_name)s_%(referred_table_name)s",
     "pk": "pk_%(table_name)s"
 }
-app.json.compact = False
-api = Api(app)
 metadata_obj = MetaData(naming_convention=convention)
 db = SQLAlchemy(metadata=metadata_obj)
 db.init_app(app)
 
+# Configure Flask-Session to use the SQLAlchemy instance
+app.config['SESSION_SQLALCHEMY'] = db
+Session(app)  # Call Session(app) only once
+
+# Continue with the rest of your configuration
+bcrypt = Bcrypt(app)
+api = Api(app)
 migrate = Migrate(app, db)
