493289c3f99680e59fcc6a8108dc0572f58d2900
Jacob <jacobpayne007@gmail.com>
2025-02-20 21:25:14 -0600

    update insights charts

--- client/src/Components/InsightsComponent.js
+++ client/src/Components/InsightsComponent.js
@@ -7,7 +7,7 @@ function Insights() {
   const { categories } = useContext(AppContext);
   return (
   <div className='bank_account_container' style={{flexWrap:"wrap"}}>
-    <div className="transaction_headers" style={{gridTemplateColumns:"3fr 2fr", width:"100%"}}>
+    <div className="transaction_headers" style={{gridTemplateColumns:"3fr 2fr", width:"100%",fontSize:"1.5rem"}}>
       <h4>Category</h4>
       <h4>Amount</h4>
     </div>
@@ -16,8 +16,8 @@ function Insights() {
         return(
         <div style={{width:"100%"}}key={key} >
           <div className='transaction_rows'  style={{gridTemplateColumns: "3fr 2fr"}}>
-            <p id='transaction_name'>{key}</p>
-            <p id='Price'><span id='DollaBill'>$ </span>{value}</p>
+            <p id='transaction_name' style = {{fontSize:"1.2rem"}}>{key}</p>
+            <p id='Price' style = {{fontSize:"1.2rem"}}><span id='DollaBill'>$ </span>{value}</p>
           </div>
         </div>)
       })
